10372b6ea870773514793ac8d44d7889336c38aa
nebislol <jm-polidor@hotmail.com>
2023-01-22 04:15:23 -0300

    ultima edicion entrega final

--- src/componentes/cart/cart.js
+++ src/componentes/cart/cart.js
@@ -12,11 +12,12 @@ servicioContenedor.addEventListener('click', (e) => {
 const validarServicioEnCarrito = (servicioId) => {
     const servicioRepetido = carrito.find(servicios => servicios.id == servicioId);
     if (!servicioRepetido) {
-        const servicio = servicios.find(servicios => servicios.id == servicioId);
+        servicio = servicios.find(servicios => servicios.id == servicioId);
         carrito.push(servicio);
         alertaServicioAgregado();
         pintarServicioCarrito(servicio);
         guardarCarritoStorage(carrito);
+
 
     } else {
         alertaServicioRepetido();
@@ -103,3 +104,71 @@ const obtenerCarritoStorage = () => {
     const carritoStorage = JSON.parse(localStorage.getItem('carrito'));
     return carritoStorage;
 };
+
+
+
+
+//capturar informacion del formulario
+const formulario = document.querySelector('#forms');
+
+formulario.addEventListener('submit', () =>{
+    event.preventDefault();
+
+    let capturarNombre =document.querySelector('#formularioNombre').value;
+    let capturaremail =document.querySelector('#formularioEmail').value;
+    if(capturaremail.length == 0) {
+        alertaCompletaFormulario();
+        return;}
+    let capturartelefono =document.querySelector('#formularioTelefono').value;
+    let capturarmensaje =document.querySelector('#formularioMensaje').value;
+
+    class Clientes {
+        constructor(nombre, email, telefono, mensaje) {
+            this.nombre = nombre;
+            this.email = email;
+            this.telefono = telefono;
+            this.mensaje = mensaje;
+        }
+    };
+    
+    cliente1 = new Clientes( capturarNombre, capturaremail,capturartelefono, capturarmensaje);
+    
+    agregar(capturaremail);    
+    
+});
+
+const alertaCompletaFormulario = () => {
+    Swal.fire({
+        position: 'top-end',
+        icon: 'warning',
+        title: 'El formulario esta vacio debes completar almenos el email',
+        showConfirmButton: false,
+        timer: 1500
+    })
+};
+
+let baseClientes = [];
+const agregar = (capturaremail) =>{
+    const clienteRepetido = baseClientes.find(baseClientes => baseClientes.email == capturaremail);
+    if (!clienteRepetido) {
+        const nuevoCliente = baseClientes.find(baseClientes => baseClientes.email == capturaremail);
+        baseClientes.push(cliente1);
+        alertaServicioAgregado();
+        pedidoPresupuesto();
+        
+
+        
+    } else {
+        alertaServicioRepetido();
+    }
+    
+};
+
+
+//Unificar servicios agregados al carrito con datos ingresados en el formulario
+const pedidoPresupuesto = () => {
+    const pedidoPresupuestoRecibido = [...carrito, ...baseClientes]
+    console.log(pedidoPresupuestoRecibido);
+};
+
+
